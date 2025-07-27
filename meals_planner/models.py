abaf82f0bc572a9cb1e14f12761db28a8530a52e
Mbelenga <mbelengaterence@gmail.com>
2025-07-28 02:16:17 +0300

    Ameal class

--- meals_planner/models.py
+++ meals_planner/models.py
@@ -3,6 +3,11 @@ from django.db import models
 class Meals(models.Model):
     """Different kind of meals and also planning part"""
     name = models.CharField(max_length=100)
+    """
+    This creates a column in the table called name. 
+    It's a text field limited to 100 characters. 
+    CharField is used for short strings.
+    """
     calories = models.IntegerField()
     is_vegetarian = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
