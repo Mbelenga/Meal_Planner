47653ea8b3f226e834061e370f8991c542e1bfc4
Mbelenga <mbelengaterence@gmail.com>
2025-07-28 02:06:32 +0300

    Meal Planning

---
+++ meals_planner/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MealsPlannerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meals_planner'
---
+++ meals_planner/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class Meals(models.Model):
+    ""'Different kind of meals and also planning part"""
