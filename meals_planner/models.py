03e0a330ce14bd64be01cee9710131dd1073d6fd
Mbelenga <mbelengaterence@gmail.com>
2025-07-28 02:11:27 +0300

    A meal class

--- meals_planner/models.py
+++ meals_planner/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class Meals(models.Model):
-    ""'Different kind of meals and also planning part"""
+    """Different kind of meals and also planning part"""
+    name = models.CharField(max_length=100)
+    calories = models.IntegerField()
+    is_vegetarian = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.name
