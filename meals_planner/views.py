fdf61be596198cbd1982b28359a746b8b3b8d84c
Mbelenga <mbelengaterence@gmail.com>
2025-07-29 02:47:05 +0300

    render

--- meals_planner/views.py
+++ meals_planner/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """ A simple Home Page for the Meal Planner """
+    return render(request, 'meals_planner/index.html')
