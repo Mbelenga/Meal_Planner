525be0e19de12d4f4a8f7cc0688f680129ec1386
Mbelenga <mbelengaterence@gmail.com>
2025-07-30 23:54:16 +0300

    Updated

--- meals_planner/admin.py
+++ meals_planner/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Meal, Ingredient
+from .models import Meal, Ingredient, MealPlan, Recipe
 
 admin.site.register(Meal)
 admin.site.register(Ingredient)
