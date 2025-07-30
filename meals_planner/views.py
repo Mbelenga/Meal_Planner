d082646e19020e83a664d7d3c197d74cb34fd7d6
Mbelenga <mbelengaterence@gmail.com>
2025-07-31 00:02:03 +0300

    created a meal plans view

--- meals_planner/views.py
+++ meals_planner/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import MealPlan
 
 def index(request):
     """ A simple Home Page for the Meal Planner """
     return render(request, 'meals_planner/index.html')
+
+def mealplan_list(request):
+    mealplans = MealPlan.objects.all  #Fetch all mealplan objects
+    return render(request, 'meals_planner/mealplans_list.html', {'mealplans':mealplans})
